75025c859c427e2f5fa731b3e4862f665cd98f7d
ShHawk <alexandreuzan9@gmail.com>
2024-02-15 01:42:27 +0100

    Update interface.h

---
+++ include/interface.h
@@ -0,0 +1,14 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+#include <string>
+
+class CryptageInterface {
+public:
+    virtual ~CryptageInterface() = default;
+
+    virtual std::string crypter(const std::string& message) = 0;
+    virtual std::string decrypter(const std::string& message) = 0;
+};
+
+#endif // INTERFACE_H
