a17470a24c23b3bedd33b6975fab16f3c6941d74
ShHawk <alexandreuzan9@gmail.com>
2024-02-15 01:29:37 +0100

    Update crypteur.h

--- include/crypteur.h
+++ include/crypteur.h
@@ -2,14 +2,14 @@
 #define CRYPTEUR_H
 
 #include <string>
+#include <memory>
 
 class Crypteur {
 public:
-    explicit Crypteur(int decalage);
-    std::string crypter(const std::string& message);
-
-private:
-    int decalage;
+    virtual ~Crypteur() = default;
+    virtual std::string crypter(const std::string& message) = 0;
 };
+
+std::unique_ptr<Crypteur> createCrypteur(const std::string& type, int key);
 
 #endif // CRYPTEUR_H
